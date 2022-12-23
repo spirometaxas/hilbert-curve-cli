c42a92a5b3602e3b7f14063d9f16558e722839cf
Spiro Metaxas <spirometaxas@gmail.com>
2022-12-22 20:48:36 -0500

    Version 1.2.0

--- src/cli.js
+++ src/cli.js
@@ -1,21 +1,92 @@
 #!/usr/bin/env node
 const hilbert = require('./index.js');
 
-const printUsage = function() {
-    console.log('\nUsage:\n' + '  $ hilbert-curve-cli <n>\n');
+const printUsage = function(showIntro) {
+    if (showIntro) {
+        console.log('\n Print the Hilbert Curve to the console!');
+    }
+    console.log('\n' + 
+                ' Usage:\n' + 
+                '   $ hilbert-curve-cli <n>\n' + 
+                '\n' + 
+                ' Options:\n' + 
+                '   --line=<line>      Draw using a specific line type: [bold|double|standard]\n' + 
+                '   --rotate=<rotate>  Rotate the fractal: [left|right|flip|standard]\n');
+}
+
+const getFlags = function(params) {
+    let flags = [];
+    if (params) {
+        for (let i = 0; i < params.length; i++) {
+            if (params[i].startsWith('-')) {
+                flags.push(params[i]);
+            }
+        }
+    }
+    return flags;
+}
+
+const getValues = function(params) {
+    let values = [];
+    if (params) {
+        for (let i = 0; i < params.length; i++) {
+            if (!params[i].startsWith('-')) {
+                values.push(params[i]);
+            }
+        }
+    }
+    return values;
+}
+
+const getLine = function(flags) {
+    for (let i = 0; i < flags.length; i++) {
+        if (flags[i] && flags[i].toLowerCase().startsWith('--line=')) {
+            const line = flags[i].substring(7);
+            if (line) {
+                if (line.toLowerCase() === 'bold' || line.toLowerCase() === 'double' || line.toLowerCase() === 'standard') {
+                    return line.toLowerCase();
+                } else {
+                    console.log('\n Warning: Please provide a supported line type: [bold|double|standard]');
+                }
+            } else {
+                console.log('\n Warning: Please provide a supported line type: [bold|double|standard]');
+            }
+        }
+    }
+    return undefined;
+}
+
+const getRotation = function(flags) {
+    for (let i = 0; i < flags.length; i++) {
+        if (flags[i] && flags[i].toLowerCase().startsWith('--rotate=')) {
+            const line = flags[i].substring(9);
+            if (line) {
+                if (line.toLowerCase() === 'left' || line.toLowerCase() === 'right' || line.toLowerCase() === 'flip' || line.toLowerCase() === 'standard') {
+                    return line.toLowerCase();
+                } else {
+                    console.log('\n Warning: Please provide a supported rotation type: [left|right|flip|standard]');
+                }
+            } else {
+                console.log('\n Warning: Please provide a supported rotation type: [left|right|flip|standard]');
+            }
+        }
+    }
+    return undefined;
 }
 
 if (process.argv.length > 2) {
     const params = process.argv.slice(2);
-    if (params[0] && !isNaN(params[0]) && parseInt(params[0]) >= 1) {
-        var n = parseInt(params[0]);
+    const values = getValues(params);
+    const flags = getFlags(params);
+    if (values[0] && !isNaN(values[0]) && parseInt(values[0]) >= 1) {
+        var n = parseInt(values[0]);
         if (n !== undefined) {
-            console.log(hilbert.create(n));
+            console.log(hilbert.create(n, { rotate: getRotation(flags), line: getLine(flags) }));
         }
     } else {
-        console.log('\n<n> should be a number greater than or equal to 1');
-        printUsage();
+        console.log('\n <n> should be a number greater than or equal to 1');
+        printUsage(false);
     }
 } else {
-    printUsage();
+    printUsage(true);
 }
--- src/index.js
+++ src/index.js
@@ -12,6 +12,60 @@ const getHeight = function(n) {
   return Math.pow(2, n);
 }
 
+const LINES = {
+  '─': {
+    STANDARD: '─',
+    BOLD: '━',
+    DOUBLE: '═',
+  },
+  '│': {
+    STANDARD: '│',
+    BOLD: '┃',
+    DOUBLE: '║',
+  },
+  '┌': {
+    STANDARD: '┌',
+    BOLD: '┏',
+    DOUBLE: '╔',
+  },
+  '┐': {
+    STANDARD: '┐',
+    BOLD: '┓',
+    DOUBLE: '╗',
+  },
+  '┘': {
+    STANDARD: '┘',
+    BOLD: '┛',
+    DOUBLE: '╝',
+  },
+  '└': {
+    STANDARD: '└',
+    BOLD: '┗',
+    DOUBLE: '╚',
+  },
+};
+
+const getLine = function(lineId, lineType) {
+  if (LINES[lineId] !== undefined && lineType !== undefined) {
+    return LINES[lineId][lineType.toUpperCase()];
+  } else if (LINES[lineId] !== undefined) {
+    return LINES[lineId].STANDARD;
+  } else {
+    return ' ';
+  }
+}
+
+const getLineType = function(line) {
+  if (line !== undefined && (line.toLowerCase() === 'standard' || line.toLowerCase() === 'double' || line.toLowerCase() === 'bold')) {
+    return line.toLowerCase();
+  }
+  return 'standard';
+}
+
+const isValidRotation = function(rotation) {
+  return rotation !== undefined && (rotation.toLowerCase() === 'left' || rotation.toLowerCase() === 'right' || rotation.toLowerCase() === 'flip' || rotation.toLowerCase() === 'standard');
+}
+
 const createBoard = function(w, h) {
   let board = [];
   for (let i = 0; i < h; i++) {
@@ -25,7 +79,7 @@ const createBoard = function(w, h) {
 }
 
 const drawCurve = function(board, pos, type, params) {
-  if (type === 'A') {
+  if (type === 'standard') {
     board[pos.y + 1][pos.x] = '┌';
     board[pos.y + 1][pos.x + 1] = '─';
     board[pos.y + 1][pos.x + 2] = '┐';
@@ -47,7 +101,7 @@ const drawCurve = function(board, pos, type, params) {
         board[pos.y][pos.x + 3] = '─';
       }
     }
-  } else if (type === 'B') {
+  } else if (type === 'left') {
     board[pos.y + 1][pos.x] = '┌';
     board[pos.y + 1][pos.x + 1] = '─';
     board[pos.y][pos.x] = '└';
@@ -70,7 +124,7 @@ const drawCurve = function(board, pos, type, params) {
     if (params && params.end2 === 'down') {
       board[pos.y][pos.x + 2] = '┐';
     }
-  } else if (type === 'C') {
+  } else if (type === 'flip') {
     board[pos.y][pos.x] = '└';
     board[pos.y][pos.x + 1] = '─';
     board[pos.y][pos.x + 2] = '┘';
@@ -89,7 +143,7 @@ const drawCurve = function(board, pos, type, params) {
     if (params && params.end2 === 'up') {
       board[pos.y + 1][pos.x + 2] = '│';
     }
-  } else if (type === 'D') {
+  } else if (type === 'right') {
     board[pos.y + 1][pos.x + 2] = '┐';
     board[pos.y + 1][pos.x + 1] = '─';
     board[pos.y][pos.x + 2] = '┘';
@@ -113,49 +167,63 @@ const hilbert = function(n, board, pos, type, params) {
   if (n <= 1) {
     drawCurve(board, pos, type, params);
   } else {
-    if (type === 'A') {
-      hilbert(n - 1, board, { x: pos.x, y: pos.y + getHeight(n - 1) }, 'A', { end1: 'down', end2: 'right' });
-      hilbert(n - 1, board, { x: pos.x + getWidth(n - 1) + 1, y: pos.y + getHeight(n - 1) }, 'A', { end1: 'left', end2: 'down' });
-      hilbert(n - 1, board, { x: pos.x, y: pos.y }, 'D', { end1: 'up', end2: params.end1 });
-      hilbert(n - 1, board, { x: pos.x + getWidth(n - 1) + 1, y: pos.y }, 'B', { end1: 'up', end2: params.end2 });
-    } else if (type === 'B') {
-      hilbert(n - 1, board, { x: pos.x, y: pos.y + getHeight(n - 1) }, 'B', { end1: 'right', end2: 'down' });
-      hilbert(n - 1, board, { x: pos.x + getWidth(n - 1) + 1, y: pos.y + getHeight(n - 1) }, 'C', { end1: 'left', end2: params.end1 });
-      hilbert(n - 1, board, { x: pos.x, y: pos.y }, 'B', { end1: 'up', end2: 'right' });
-      hilbert(n - 1, board, { x: pos.x + getWidth(n - 1) + 1, y: pos.y }, 'A', { end1: 'left', end2: params.end2 });
-    } else if (type === 'C') {
-      hilbert(n - 1, board, { x: pos.x, y: pos.y + getHeight(n - 1) }, 'D', { end1: params.end1, end2: 'down' });
-      hilbert(n - 1, board, { x: pos.x + getWidth(n - 1) + 1, y: pos.y + getHeight(n - 1) }, 'B', { end1: params.end2, end2: 'down' });
-      hilbert(n - 1, board, { x: pos.x, y: pos.y }, 'C', { end1: 'up', end2: 'right' });
-      hilbert(n - 1, board, { x: pos.x + getWidth(n - 1) + 1, y: pos.y }, 'C', { end1: 'left', end2: 'up' });
-    } else if (type === 'D') {
-      hilbert(n - 1, board, { x: pos.x, y: pos.y + getHeight(n - 1) }, 'C', { end1: params.end1, end2: 'right' });
-      hilbert(n - 1, board, { x: pos.x + getWidth(n - 1) + 1, y: pos.y + getHeight(n - 1) }, 'D', { end1: 'left', end2: 'down' });
-      hilbert(n - 1, board, { x: pos.x, y: pos.y }, 'A', { end1: params.end2, end2: 'right' });
-      hilbert(n - 1, board, { x: pos.x + getWidth(n - 1) + 1, y: pos.y }, 'D', { end1: 'up', end2: 'left' });
+    if (type === 'standard') {
+      hilbert(n - 1, board, { x: pos.x, y: pos.y + getHeight(n - 1) }, 'standard', { end1: 'down', end2: 'right' });
+      hilbert(n - 1, board, { x: pos.x + getWidth(n - 1) + 1, y: pos.y + getHeight(n - 1) }, 'standard', { end1: 'left', end2: 'down' });
+      hilbert(n - 1, board, { x: pos.x, y: pos.y }, 'right', { end1: 'up', end2: params.end1 });
+      hilbert(n - 1, board, { x: pos.x + getWidth(n - 1) + 1, y: pos.y }, 'left', { end1: 'up', end2: params.end2 });
+    } else if (type === 'left') {
+      hilbert(n - 1, board, { x: pos.x, y: pos.y + getHeight(n - 1) }, 'left', { end1: 'right', end2: 'down' });
+      hilbert(n - 1, board, { x: pos.x + getWidth(n - 1) + 1, y: pos.y + getHeight(n - 1) }, 'flip', { end1: 'left', end2: params.end1 });
+      hilbert(n - 1, board, { x: pos.x, y: pos.y }, 'left', { end1: 'up', end2: 'right' });
+      hilbert(n - 1, board, { x: pos.x + getWidth(n - 1) + 1, y: pos.y }, 'standard', { end1: 'left', end2: params.end2 });
+    } else if (type === 'flip') {
+      hilbert(n - 1, board, { x: pos.x, y: pos.y + getHeight(n - 1) }, 'right', { end1: params.end1, end2: 'down' });
+      hilbert(n - 1, board, { x: pos.x + getWidth(n - 1) + 1, y: pos.y + getHeight(n - 1) }, 'left', { end1: params.end2, end2: 'down' });
+      hilbert(n - 1, board, { x: pos.x, y: pos.y }, 'flip', { end1: 'up', end2: 'right' });
+      hilbert(n - 1, board, { x: pos.x + getWidth(n - 1) + 1, y: pos.y }, 'flip', { end1: 'left', end2: 'up' });
+    } else if (type === 'right') {
+      hilbert(n - 1, board, { x: pos.x, y: pos.y + getHeight(n - 1) }, 'flip', { end1: params.end1, end2: 'right' });
+      hilbert(n - 1, board, { x: pos.x + getWidth(n - 1) + 1, y: pos.y + getHeight(n - 1) }, 'right', { end1: 'left', end2: 'down' });
+      hilbert(n - 1, board, { x: pos.x, y: pos.y }, 'standard', { end1: params.end2, end2: 'right' });
+      hilbert(n - 1, board, { x: pos.x + getWidth(n - 1) + 1, y: pos.y }, 'right', { end1: 'up', end2: 'left' });
     }
   }
 }
 
-const draw = function(board) {
+const draw = function(board, lineType) {
   var result = '\n ';
   for (let i = 0; i < board.length; i++) {
     for (let j = 0; j < board[i].length; j++) {
-      result += board[board.length - i - 1][j];
+      result += getLine(board[board.length - i - 1][j], lineType);
     }
     result += '\n ';
   }
   return result;
 }
 
-const create = function(n) {
+const create = function(n, config) {
   if (n === undefined || isNaN(n) || n < 1) {
     return '';
   }
 
   const board = createBoard(getWidth(n), getHeight(n));
-  hilbert(n, board, { x: 0, y: 0 }, 'A', n === 1 ? { end1: 'down', end2: 'down' } : { end1: 'left', end2: 'right' });
-  return draw(board);
+
+  const rotate = config !== undefined && isValidRotation(config.rotate) ? config.rotate.toLowerCase() : 'standard';
+  const lineType = config !== undefined ? getLineType(config.line) : undefined;
+  let params = {};
+  if (rotate === 'flip') {
+    params = n === 1 ? { end1: 'up', end2: 'up' } : { end1: 'left', end2: 'right' };
+  } else if (rotate === 'left') {
+    params = n === 1 ? { end1: 'right', end2: 'right' } : { end1: 'up', end2: 'down' };
+  } else if (rotate === 'right') {
+    params = n === 1 ? { end1: 'left', end2: 'left' } : { end1: 'up', end2: 'down' };
+  } else {
+    params = n === 1 ? { end1: 'down', end2: 'down' } : { end1: 'left', end2: 'right' };
+  }
+
+  hilbert(n, board, { x: 0, y: 0 }, rotate, params);
+  return draw(board, lineType);
 }
 
 module.exports = {
